38916719c3c47c0e26944c9ec3e701c82eeed3a4
修安 李 <billy72325@gmail.com>
2022-06-04 13:47:43 +0800

    1

---
+++ NHRI/Controllers/LanguageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+namespace NHRI.Controllers
+{
+    public class LanguageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Change(string LanguageAbb)
+        {
+            if (LanguageAbb != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(LanguageAbb);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(LanguageAbb);
+            }
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = LanguageAbb;
+            Response.Cookies.Add(cookie);
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+
+    }
+}
---
+++ NHRI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NHRI.MvcApplication" Language="C#" %>
